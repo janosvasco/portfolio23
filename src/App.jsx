ff16d3ef1fee7386471c737113f52250cd054be9
Sandor Janos Vasco <janosvasco@gmail.com>
2023-07-30 17:14:51 +0200

    creating Router in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './App.css'
+import Layout from './components/Layout';
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      Janos
-    </>
-  )
+export default function App() {
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />
+  }
+]);
+  return <RouterProvider router={router} />;
 }
 
-export default App
+
