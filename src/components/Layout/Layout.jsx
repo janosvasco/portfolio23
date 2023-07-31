90dfbc16022f51eb14ae7ac28e58311cc3a1a047
Sandor Janos Vasco <janosvasco@gmail.com>
2023-07-31 15:37:03 +0200

    creating sections on Layout, inserting Navbar

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,7 +1,15 @@
-export default function Layout () {
+import './layout.css'
+import Navbar from "../Right-side/Navbar";
+import { navTexts } from "../../constants"
+
+export default function Layout() {
     return (
         <>
             Janos Vasco Portfolio
+            {navTexts.map((text, id) => {
+                return <div className="section" id={text} key={id} data-label={text}>{text}</div>
+            })}
+            <Navbar />
         </>
     )
 }
