9465e38444006a6f5d52d72341b05b4bae451935
Sandor Janos Vasco <janosvasco@gmail.com>
2023-08-01 22:52:24 +0200

    importing Header to Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,15 +1,24 @@
 import './layout.css'
 import Navbar from "../Right-side/Navbar";
 import { navTexts } from "../../constants"
+import Header from '../Header/Header';
 
 export default function Layout() {
+
+
     return (
         <>
-            Janos Vasco Portfolio
+            <Header />
             {navTexts.map((text, id) => {
-                return <div className="section" id={text} key={id} data-label={text}>{text}</div>
+                return (
+                    <div
+                        className="section"
+                        id={text}
+                        key={id}
+                        data-label={text}><p>{text}</p>
+                    </div>)
             })}
             <Navbar />
         </>
     )
-}
+} 
