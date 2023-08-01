43be1c6cc11a66cf09b7fbffd2142a95f9423a01
Sandor Janos Vasco <janosvasco@gmail.com>
2023-08-01 09:22:22 +0200

    providing event for touchscreen mobiles

--- src/components/Right-side/Navbar.jsx
+++ src/components/Right-side/Navbar.jsx
@@ -3,35 +3,47 @@ import { navTexts } from '../../constants';
 import './navbar.css';
 
 export default function Navbar() {
-    const [classStates, setClassStates] = useState(
-        navTexts.map(() => 'nav-link')
-    );
+  const [classStates, setClassStates] = useState(navTexts.map(() => 'nav-link'));
 
-    const handleMouseEnter = (id) => {
-        const updatedStates = [...classStates];
-        updatedStates[id] = 'nav-link nav-link-selected';
-        setClassStates(updatedStates);
-    };
+  const handleMouseEnter = (id) => {
+    const updatedStates = [...classStates];
+    updatedStates[id] = 'nav-link nav-link-selected';
+    setClassStates(updatedStates);
+  };
 
-    const handleMouseLeave = (id) => {
-        const updatedStates = [...classStates];
-        updatedStates[id] = 'nav-link';
-        setClassStates(updatedStates);
-    };
+  const handleMouseLeave = (id) => {
+    const updatedStates = [...classStates];
+    updatedStates[id] = 'nav-link';
+    setClassStates(updatedStates);
+  };
 
-    return (
-        <nav>
-            {navTexts.map((text, id) => (
-                <div className='nav-item' key={id} data-for-section={text}>
-                    <a
-                        href={'#' + text}
-                        className={classStates[id]}
-                        onMouseEnter={() => handleMouseEnter(id)}
-                        onMouseLeave={() => handleMouseLeave(id)}
-                    ></a>
-                    <span className='nav-label'>{text}</span>
-                </div>
-            ))}
-        </nav>
-    );
+  const handleTouchStart = (id) => {
+    const updatedStates = [...classStates];
+    updatedStates[id] = 'nav-link nav-link-selected';
+    setClassStates(updatedStates);
+  };
+
+  const handleTouchEnd = (id) => {
+    const updatedStates = [...classStates];
+    updatedStates[id] = 'nav-link';
+    setClassStates(updatedStates);
+  };
+
+  return (
+    <nav>
+      {navTexts.map((text, id) => (
+        <div className='nav-item' key={id} data-for-section={text}>
+          <a
+            href={'#' + text}
+            className={classStates[id]}
+            onMouseEnter={() => handleMouseEnter(id)}
+            onMouseLeave={() => handleMouseLeave(id)}
+            onTouchStart={() => handleTouchStart(id)}
+            onTouchEnd={() => handleTouchEnd(id)}
+          ></a>
+          <span className='nav-label'>{text}</span>
+        </div>
+      ))}
+    </nav>
+  );
 }
