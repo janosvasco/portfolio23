4cf51d26c2b25a03ef33a3ab364cd3d81d1469f2
Sandor Janos Vasco <janosvasco@gmail.com>
2023-08-09 15:32:57 +0200

    formating Navbar jsx

--- src/components/Right-side/Navbar.jsx
+++ src/components/Right-side/Navbar.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
-import { Link } from 'react-scroll'
-import { navTexts } from '../../constants'
-import './navbar.css'
+import React, { useState } from "react";
+import { Link } from "react-scroll";
+import { navTexts } from "../../constants";
+import "./navbar.css";
 
 export default function Navbar() {
   const [activeLinkIndex, setActiveLinkIndex] = useState(null);
@@ -9,7 +9,7 @@ export default function Navbar() {
     <nav>
       {navTexts.map((text, id) => (
         <div
-          className={`nav-item ${id === activeLinkIndex ? 'active' : ''}`}
+          className={`nav-item ${id === activeLinkIndex ? "active" : ""}`}
           key={id}
           data-for-section={text}
         >
@@ -19,14 +19,21 @@ export default function Navbar() {
             smooth={true}
             offset={0} // Adjust this offset value to consider the height of your sticky navbar
             duration={300} // Adjust the scrolling duration as needed
-            activeClass={`nav-link-selected${id === 3 ? '-white' : ''}`}
+            activeClass={`nav-link-selected${
+              id === 3 || id === 1 ? "-white" : ""
+            }`}
             className="nav-link"
             onClick={() => setActiveLinkIndex(id)}
           ></Link>
-            <span className={`nav-label ${activeLinkIndex === id && id === 3 ? 'nav-label-white' : ''}`}>{text}</span>
-
+          <span
+            className={`nav-label ${
+              activeLinkIndex === id && id === 3 ? "nav-label-white" : ""
+            }`}
+          >
+            {text}
+          </span>
         </div>
       ))}
     </nav>
-  )
+  );
 }
