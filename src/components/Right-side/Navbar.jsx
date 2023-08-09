8201ae44f86959923c745fb6ff6f0d45af462723
Sandor Janos Vasco <janosvasco@gmail.com>
2023-08-09 12:09:40 +0200

    updating Navbar css and jsx

--- src/components/Right-side/Navbar.jsx
+++ src/components/Right-side/Navbar.jsx
@@ -1,23 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-scroll'
 import { navTexts } from '../../constants'
 import './navbar.css'
 
-export default function Navbar () {
+export default function Navbar() {
+  const [activeLinkIndex, setActiveLinkIndex] = useState(null);
   return (
     <nav>
       {navTexts.map((text, id) => (
-        <div className='nav-item' key={id} data-for-section={text}>
+        <div
+          className={`nav-item ${id === activeLinkIndex ? 'active' : ''}`}
+          key={id}
+          data-for-section={text}
+        >
           <Link
             to={text}
             spy={true}
             smooth={true}
             offset={0} // Adjust this offset value to consider the height of your sticky navbar
             duration={300} // Adjust the scrolling duration as needed
-            activeClass="nav-link-selected"
+            activeClass={`nav-link-selected${id === 3 ? '-white' : ''}`}
             className="nav-link"
+            onClick={() => setActiveLinkIndex(id)}
           ></Link>
-          <span className='nav-label'>{text}</span>
+            <span className={`nav-label ${activeLinkIndex === id && id === 3 ? 'nav-label-white' : ''}`}>{text}</span>
+
         </div>
       ))}
     </nav>
