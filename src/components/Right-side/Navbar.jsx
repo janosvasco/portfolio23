8e7b59293c8b8727999c7f9854f39ec66c788216
Sandor Janos Vasco <janosvasco@gmail.com>
2023-08-11 14:22:26 +0200

    updating Navbar features

--- src/components/Right-side/Navbar.jsx
+++ src/components/Right-side/Navbar.jsx
@@ -1,10 +1,11 @@
+/* import "./navbar.css";
 import React, { useState } from "react";
 import { Link } from "react-scroll";
 import { navTexts } from "../../constants";
-import "./navbar.css";
 
 export default function Navbar() {
   const [activeLinkIndex, setActiveLinkIndex] = useState(null);
+
   return (
     <nav>
       {navTexts.map((text, id) => (
@@ -17,17 +18,71 @@ export default function Navbar() {
             to={text}
             spy={true}
             smooth={true}
-            offset={0} // Adjust this offset value to consider the height of your sticky navbar
-            duration={300} // Adjust the scrolling duration as needed
+            offset={0}
+            duration={300}
             activeClass={`nav-link-selected${
               id === 3 || id === 1 ? "-white" : ""
             }`}
             className="nav-link"
-            onClick={() => setActiveLinkIndex(id)}
+            onClick={() => {
+              setActiveLinkIndex(id);
+            }}
           ></Link>
           <span
             className={`nav-label ${
-              activeLinkIndex === id && id === 3 ? "nav-label-white" : ""
+              (id === 1 || id === 3) && id === activeLinkIndex
+                ? "nav-label-white"
+                : ""
+            }`}
+          >
+            {text}
+          </span>
+        </div>
+      ))}
+    </nav>
+  );
+}
+ */
+
+import "./navbar.css";
+import React, { useState } from "react";
+import { Link } from "react-scroll";
+import { navTexts } from "../../constants";
+
+export default function Navbar() {
+  const [activeLinkIndex, setActiveLinkIndex] = useState(null);
+
+  return (
+    <nav
+      className={`nav-hover-white-${
+        activeLinkIndex === 1 || activeLinkIndex === 3 ? "active" : "inactive"
+      }`}
+    >
+      {navTexts.map((text, id) => (
+        <div
+          className={`nav-item ${id === activeLinkIndex ? "active" : ""}`}
+          key={id}
+          data-for-section={text}
+        >
+          <Link
+            to={text}
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={300}
+            activeClass={`nav-link-selected${
+              id === 3 || id === 1 ? "-white" : ""
+            }`}
+            className="nav-link"
+            onClick={() => {
+              setActiveLinkIndex(id);
+            }}
+          ></Link>
+          <span
+            className={`nav-label ${
+              (id === 1 || id === 3) && id === activeLinkIndex
+                ? "nav-label-white"
+                : ""
             }`}
           >
             {text}
