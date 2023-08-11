6948e2b07385dcefe6d1d60e0852e15376814c62
Sandor Janos Vasco <janosvasco@gmail.com>
2023-08-11 16:22:35 +0200

    finalizing Navbar scrolling behaviour

--- src/components/Right-side/Navbar.jsx
+++ src/components/Right-side/Navbar.jsx
@@ -1,56 +1,39 @@
-/* import "./navbar.css";
-import React, { useState } from "react";
-import { Link } from "react-scroll";
-import { navTexts } from "../../constants";
-
-export default function Navbar() {
-  const [activeLinkIndex, setActiveLinkIndex] = useState(null);
-
-  return (
-    <nav>
-      {navTexts.map((text, id) => (
-        <div
-          className={`nav-item ${id === activeLinkIndex ? "active" : ""}`}
-          key={id}
-          data-for-section={text}
-        >
-          <Link
-            to={text}
-            spy={true}
-            smooth={true}
-            offset={0}
-            duration={300}
-            activeClass={`nav-link-selected${
-              id === 3 || id === 1 ? "-white" : ""
-            }`}
-            className="nav-link"
-            onClick={() => {
-              setActiveLinkIndex(id);
-            }}
-          ></Link>
-          <span
-            className={`nav-label ${
-              (id === 1 || id === 3) && id === activeLinkIndex
-                ? "nav-label-white"
-                : ""
-            }`}
-          >
-            {text}
-          </span>
-        </div>
-      ))}
-    </nav>
-  );
-}
- */
-
 import "./navbar.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-scroll";
 import { navTexts } from "../../constants";
 
 export default function Navbar() {
   const [activeLinkIndex, setActiveLinkIndex] = useState(null);
+
+  function getSectionOffsets() {
+    return navTexts.map((text) => {
+      const section = document.getElementById(text);
+      return section.offsetTop;
+    });
+  }
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const sectionOffsets = getSectionOffsets();
+      const scrollPosition = window.scrollY + window.innerHeight / 2;
+
+      const newActiveLinkIndex = sectionOffsets.findIndex(
+        (offset, index) =>
+          scrollPosition >= offset &&
+          (!sectionOffsets[index + 1] ||
+            scrollPosition < sectionOffsets[index + 1])
+      );
+
+      setActiveLinkIndex(newActiveLinkIndex);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <nav
