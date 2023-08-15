a88771527720ab23e21827395687500d937b4470
Sandor Janos Vasco <janosvasco@gmail.com>
2023-08-15 16:49:12 +0200

    games section css update

--- src/components/Sections/Sections.jsx
+++ src/components/Sections/Sections.jsx
@@ -72,20 +72,6 @@ export default function Sections() {
           </div>
         </div>
       );
-    } else if (text === "JÁTÉKOK") {
-      return (
-        <div id="games-container">
-          <NavLink to="/tetris">
-            <div className="games-box" id="tetris-box">
-              <h2>Tetrisz</h2>
-              <div id="tetris-bg"></div>
-            </div>
-          </NavLink>
-          <div className="games-box" id="rps-box">
-            <h2>Kő-papír-olló</h2>
-          </div>
-        </div>
-      );
     } else if (text === "MOTIVÁCIÓ") {
       return (
         <>
@@ -99,6 +85,32 @@ export default function Sections() {
               gondolkodásom kamatoztatásával lenyűgöző weboldalak és
               alkalmazások készítése.
             </p>
+          </div>
+        </>
+      );
+    } else if (text === "PROJEKTEK") {
+      return (
+        <>
+          <h2 id="projects-title">Projektek</h2>
+        </>
+      );
+    } else if (text === "JÁTÉKOK") {
+      return (
+        <>
+          <h2 id="games-title">Játékok</h2>
+          <div id="games-container">
+            <NavLink to="/tetris">
+              <div className="games-box" id="tetris-box">
+                <h3 className="game-type">Tetrisz</h3>
+                {<div id="tetris-bg"></div>}
+              </div>
+            </NavLink>
+            <NavLink to="#">
+              <div className="games-box" id="rps-box">
+                <h3 className="game-type">Kő-papír-olló</h3>
+                <p>hamarosan...</p>
+              </div>
+            </NavLink>
           </div>
         </>
       );
