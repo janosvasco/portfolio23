038be3372016890837b2ae1efc9269f6126a19c1
Sandor Janos Vasco <janosvasco@gmail.com>
2023-08-18 16:40:28 +0200

    player fast drop

--- src/pages/Tetris/business/Board.jsx
+++ src/pages/Tetris/business/Board.jsx
@@ -1,4 +1,5 @@
 import { defaultCell } from "../business/Cell";
+import { movePlayer } from "./PlayerController";
 import { transferToBoard } from "./Tetrominoes";
 
 export function buildBoard({ rows, columns }) {
@@ -12,11 +13,58 @@ export function buildBoard({ rows, columns }) {
   };
 }
 
+const findDropPositon = ({ board, position, shape }) => {
+  let max = board.size.rows - position.row + 1;
+  let row = 0;
+
+  for (let i = 0; i < max; i++) {
+    const delta = { row: i, column: 0 };
+    const result = movePlayer({ delta, position, shape, board });
+    const { collided } = result;
+
+    if (collided) {
+      break;
+    }
+
+    row = position.row + i;
+  }
+
+  return { ...position, row };
+};
+
 export const nextBoard = ({ board, player, resetPlayer, addLinesCleared }) => {
   const { tetromino, position } = player;
   let rows = board.rows.map((row) =>
     row.map((cell) => (cell.occupied ? cell : { ...defaultCell }))
   );
+
+  const dropPosition = findDropPositon({
+    board,
+    position,
+    shape: tetromino.shape,
+  });
+
+  //ghost
+  const className = `${tetromino.className} ${
+    player.isFastDropping ? "" : "ghost"
+  }`;
+  rows = transferToBoard({
+    className,
+    isOccupied: player.isFastDropping,
+    position: dropPosition,
+    rows,
+    shape: tetromino.shape,
+  });
+
+  if (!player.isFastDropping) {
+    rows = transferToBoard({
+      className: tetromino.className,
+      isOccupied: player.collided,
+      position,
+      rows,
+      shape: tetromino.shape,
+    });
+  }
 
   rows = transferToBoard({
     className: tetromino.className,
