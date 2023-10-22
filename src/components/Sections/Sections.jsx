3405672d348966839787ccdff4549d127c036c16
Sandor Janos Vasco <janosvasco@gmail.com>
2023-10-22 10:54:20 +0200

    gitignore, dependencies setup

--- src/components/Sections/Sections.jsx
+++ src/components/Sections/Sections.jsx
@@ -78,19 +78,16 @@ export default function Sections() {
       );
     } else if (text === "MOTIVÁCIÓ") {
       return (
-        <>
-          <div className="text-box">
-            <p>
-              Képző- és alkalmazott művészeti tanulmányaimat 2019- ben fejeztem
-              be. Azóta művészi karrieremre koncentráltam, közben értékes
-              munkatapasztalatot is szereztem a magyarországi Indonéz
-              Nagykövetségen a Nagykövet asszisztenseként. Az elmúlt évek során
-              elhatároztam, hogy célom a kreativitásom és az analitikus
-              gondolkodásom kamatoztatásával weboldalak és alkalmazások
-              készítése.
-            </p>
-          </div>
-        </>
+        <div className="text-box">
+          <p>
+            Képző- és alkalmazott művészeti tanulmányaimat 2019- ben fejeztem
+            be. Azóta művészi karrieremre koncentráltam, közben értékes
+            munkatapasztalatot is szereztem a magyarországi Indonéz
+            Nagykövetségen a Nagykövet asszisztenseként. Az elmúlt évek során
+            elhatároztam, hogy célom a kreativitásom és az analitikus
+            gondolkodásom kamatoztatásával weboldalak és alkalmazások készítése.
+          </p>
+        </div>
       );
     } else if (text === "PROJEKTEK") {
       return (
