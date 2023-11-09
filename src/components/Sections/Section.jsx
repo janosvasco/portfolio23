187c1e2fca037f9793ba4d5c4d91707d3f11e67c
Sandor Janos Vasco <janosvasco@gmail.com>
2023-11-09 21:38:49 +0100

    creating Section component with function

---
+++ src/components/Sections/Section.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Header from "./Header/Header";
+import Cv from "./Main/Cv";
+import Motivation from "./Main/Motivation";
+import Projects from "./Main/Projects";
+import Games from "./Main/Games";
+
+export default function Section({ children, ...props }) {
+  function renderSectionContent(text) {
+    if (text === "FEJLÉC") {
+      return <Header />;
+    } else if (text === "CV") {
+      return <Cv />;
+    } else if (text === "MOTIVÁCIÓ") {
+      return <Motivation />;
+    } else if (text === "PROJEKTEK") {
+      return <Projects />;
+    } else if (text === "JÁTÉKOK") {
+      return <Games />;
+    }
+  }
+
+  return <section {...props}>{renderSectionContent(props.id)}</section>;
+}
