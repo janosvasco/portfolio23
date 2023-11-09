ba9cd9a06d10a548b223c763b2472bc3f46d02bc
Sandor Janos Vasco <janosvasco@gmail.com>
2023-11-09 21:37:50 +0100

    renaming files

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import "./App.css";
 import React from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App2 from "./pages/Tetris/App2";
-import Sections from "./components/Sections/Sections";
+import Layout from "./components/Sections/Layout";
 
 export default function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Sections />,
+      element: <Layout />,
     },
     {
       path: "/tetris",
