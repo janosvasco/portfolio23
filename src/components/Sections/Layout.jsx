20918f5bc669d829fba97b1d3a1b659b250881ca
Sandor Janos Vasco <janosvasco@gmail.com>
2023-11-09 21:38:20 +0100

    simplifying Layout structure

---
+++ src/components/Sections/Layout.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Layout.css";
+import { navTexts } from "../../constants";
+import Navbar from "./Right-side/Navbar";
+import Section from "./Section";
+
+export default function Sections() {
+  const sections = navTexts.map((text) => {
+    return (
+      <Section className="section" id={text} key={text} data-label={text} />
+    );
+  });
+
+  const headerSection = sections.find(
+    (section) => section.props.id === "FEJLÉC"
+  );
+
+  const mainSections = sections.filter(
+    (section) => section.props.id !== "FEJLÉC"
+  );
+
+  return (
+    <>
+      <header>{headerSection}</header>
+      <Navbar />
+      <main>{mainSections}</main>
+    </>
+  );
+}
