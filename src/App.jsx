a3fc449f65f1cae42503d2ee8e3bde04a1b853e1
Sandor Janos Vasco <janosvasco@gmail.com>
2023-11-09 19:48:49 +0100

    deleting unnecessary Layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
 import React from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Layout from "./components/Layout/Layout";
 import App2 from "./pages/Tetris/App2";
+import Sections from "./components/Sections/Sections";
 
 export default function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Layout />,
+      element: <Sections />,
     },
     {
       path: "/tetris",
