e4346b5cc13f795015aaffeba979b0b1e9962b82
Sandor Janos Vasco <janosvasco@gmail.com>
2023-11-10 13:31:21 +0100

    Games component css grid update

--- src/components/Sections/Main/Games/GameItem.jsx
+++ src/components/Sections/Main/Games/GameItem.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-export default function GameItem({ children, link, id, title }) {
+export default function GameItem({ link, id, title }) {
   return (
     <NavLink to={link}>
       <div className="games-box" id={id}>
         <h3 className="game-type">{title}</h3>
-        {<div id="tetris-bg"></div>}
-        {children}
       </div>
     </NavLink>
   );
--- src/components/Sections/Main/Games/Games.jsx
+++ src/components/Sections/Main/Games/Games.jsx
@@ -4,14 +4,14 @@ import GameItem from "./GameItem";
 
 export default function Games() {
   return (
-    <>
-      <h2 id="games-title">Alkalmazások</h2>
-      <div id="games-container">
-        <GameItem link="/tetris" id="tetris-box" title="Tetrisz" />
-        <GameItem link="#" id="rpx-box" title="Kő-papír-olló">
-          <p>hamarosan...</p>
-        </GameItem>
-      </div>
-    </>
+    <div id="games-container">
+      <h2>Alkalmazások</h2>
+      <GameItem link="/tetris" id="tetris-box" title="Tetrisz" />
+      <GameItem link="#" id="rpx-box" title="Kő-papír-olló" />
+      <GameItem link="/#" id="ttt-box" title="Tic-Tac-Toe" />
+      <GameItem link="/#" id="quiz-box" title="Quiz" />
+      <GameItem link="/#" id="inv-calc-box" title="Investment Calculator" />
+      <GameItem link="/#" id="placekeeper-box" title="Placekeeper" />
+    </div>
   );
 }
