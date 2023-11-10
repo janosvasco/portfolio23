1bf3b59074ce10d4d8f10bdd76a39135a591452f
Sandor Janos Vasco <janosvasco@gmail.com>
2023-11-10 14:15:09 +0100

    box shadow for app section

--- src/components/Sections/Main/Games/Games.jsx
+++ src/components/Sections/Main/Games/Games.jsx
@@ -8,7 +8,7 @@ export default function Games() {
       <h2>Alkalmazások</h2>
       <GameItem link="/tetris" id="tetris-box" title="Tetrisz" />
       <GameItem link="#" id="rpx-box" title="Kő-papír-olló" />
-      <GameItem link="/#" id="ttt-box" title="Tic-Tac-Toe" />
+      <GameItem link="/oxo" id="ttt-box" title="Tic-Tac-Toe" />
       <GameItem link="/#" id="quiz-box" title="Quiz" />
       <GameItem link="/#" id="inv-calc-box" title="Investment Calculator" />
       <GameItem link="/#" id="placekeeper-box" title="Placekeeper" />
