462265464eb36225c89eea7010d6541fbcbf15dc
Sandor Janos Vasco <janosvasco@gmail.com>
2023-11-12 22:37:55 +0100

    creating ProjectDetails component, moving logic

---
+++ src/components/Sections/Main/ProjectDetails.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { EXAMPLES } from "./EXAMPLES";
+import TabButton from "./TabButton";
+
+export default function ProjectDetails() {
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
+  let tabContent = <p>Kérlek válassz ki egy témát</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <menu>
+        <TabButton
+          isSelected={selectedTopic === "csapatmunka"}
+          onSelect={() => handleSelect("csapatmunka")}
+        >
+          Csapatmunka
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "kihívások"}
+          onSelect={() => handleSelect("kihívások")}
+        >
+          Kihívások
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "idővonal"}
+          onSelect={() => handleSelect("idővonal")}
+        >
+          Idővonal
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "dokumentáció"}
+          onSelect={() => handleSelect("dokumentáció")}
+        >
+          Dokumentáció
+        </TabButton>
+      </menu>
+      {tabContent}
+    </div>
+  );
+}
