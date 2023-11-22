0cce8ad38de44a5e3b6f181c2e39338171f9d6f5
Sandor Janos Vasco <janosvasco@gmail.com>
2023-11-22 19:25:26 +0100

    make links at Projects section

--- src/components/Sections/Main/Projects/Projects.jsx
+++ src/components/Sections/Main/Projects/Projects.jsx
@@ -1,16 +1,21 @@
-import React from "react";
 import "./Projects.css";
 import ProjectDetails from "./ProjectDetails";
 import { FRONTEND } from "./FrontendProject";
 import { FULLSTACK } from "./FullstackProject";
+import { NavLink } from "react-router-dom";
 
 export default function Projects() {
   return (
     <>
       <h2 id="projects-title">Projektek</h2>
-      <div id="frontend-box" className="project-container"></div>
+
+      <NavLink to="https://webshop-project-d5e24.web.app/">
+        <div id="frontend-box" className="project-container"></div>
+      </NavLink>
       <ProjectDetails details={FRONTEND} />
-      <div id="fullstack-box" className="project-container"></div>
+      <NavLink>
+        <div id="fullstack-box" className="project-container"></div>
+      </NavLink>
       <ProjectDetails details={FULLSTACK} />
     </>
   );
